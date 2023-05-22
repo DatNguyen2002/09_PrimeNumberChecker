532bb40dabe30b80a9365ac143930acbfca80f40
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-05-22 16:15:55 +0700

    Create Program.cs

---
+++ PrimeNumberChecker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -1;
+
+            while (number != 0)
+            {
+                Console.Write("Nhap sos can kiem tra (0 de thoat): ");
+                number = Int32.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    int i = 2;
+                    bool check = false;
+
+                    while (i <= Math.Sqrt(number))
+                    {
+                        if (number % i == 0)
+                        {
+                            check = false;
+                            break;
+                        }
+                        i++;
+                    }
+                    if(check)
+                    {
+                        Console.WriteLine("{0} la so nguyen to", number);
+                    }
+                    else { Console.WriteLine("{0} khong la so nguyen to", number); }
+                }
+            }
+
+        }
+    }
+}
